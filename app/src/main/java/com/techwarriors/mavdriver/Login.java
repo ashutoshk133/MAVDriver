2846b87a304afd9a7bf13aadb5797e4376a758cd
Ashutosh Kanade <ashutoshk133@gmail.com>
2017-11-06 16:23:57 -0600

    commit 1

--- app/src/main/java/com/techwarriors/mavdriver/Login.java
+++ app/src/main/java/com/techwarriors/mavdriver/Login.java
@@ -2,6 +2,9 @@ package com.techwarriors.mavdriver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,6 +13,13 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        TextView forgotpwd= (TextView)findViewById(R.id.forgotpwd);
 
+        forgotpwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Login.this, "Forgot", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
